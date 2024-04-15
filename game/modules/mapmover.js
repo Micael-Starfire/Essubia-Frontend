a8c7439ada4886a11f32e77182ac1939d6ed7c0b
Micael <wildstar92@yahoo.com>
2024-04-15 12:37:46 -0400

    Prevent dragging the map from selecting a tile
    Modified MapMover so that if the map is moved by more than 16 pixels, the tiles'
    normal click handler is prevented from firing. Thus, moving the map will no longer
    select a tile

--- game/modules/mapmover.js
+++ game/modules/mapmover.js
@@ -2,6 +2,9 @@ export class MapMover {
     constructor() {
         this.startX = 0;
         this.startY = 0;
+
+        this.previousX = 0;
+        this.previousY = 0;
     }
 
     handleEvent = (pEvent) => {
@@ -13,6 +16,8 @@ export class MapMover {
 
             this.startX = pEvent.clientX;
             this.startY = pEvent.clientY;
+            this.previousX = this.startX;
+            this.previousY = this.startY;
 
             document.addEventListener("mousemove", this.dragMap);
             document.addEventListener("mouseup", this.releaseMap);
@@ -21,10 +26,10 @@ export class MapMover {
 
     dragMap = (pEvent) => {
         pEvent.preventDefault();
-        let dx = pEvent.clientX - this.startX;
-        let dy = pEvent.clientY - this.startY;
-        this.startX = pEvent.clientX;
-        this.startY = pEvent.clientY;
+        let dx = pEvent.clientX - this.previousX;
+        let dy = pEvent.clientY - this.previousY;
+        this.previousX = pEvent.clientX;
+        this.previousY = pEvent.clientY;
 
         let table = document.getElementById("mapBox");
         table.scrollTop = table.scrollTop - dy;
@@ -32,10 +37,25 @@ export class MapMover {
     }
 
     releaseMap = (pEvent) => {
+        let dx = pEvent.clientX - this.startX;
+        let dy = pEvent.clientY - this.startY;
+
+        // If more than 16 pixels moved, prevent tile selection
+        if ( dx > 16 || dx < -16 || dy > 16 || dy < -16) {
+            window.addEventListener('click', this.stopClick, true);
+        }
+        
         let table = document.getElementById("mapBox");
 
         table.style.cursor = "pointer";
         document.removeEventListener("mousemove", this.dragMap);
         document.removeEventListener("mouseup", this.releaseMap);
+    }
+
+    stopClick = (pEvent) => {
+        // Executes on the capture phase
+        // Prevents the normal tile click handler from firing
+        pEvent.stopPropagation();
+        window.removeEventListener('click', this.stopClick, true);
     }
 }
