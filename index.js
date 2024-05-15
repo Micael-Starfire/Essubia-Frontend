70356e77579f43d978ce2ebe6077d902bcb70fc7
Micael <wildstar92@yahoo.com>
2024-05-15 12:37:59 -0400

    Add Advance Turn and Reset Map functions to index page
    Added buttons on the home page to advance a turn on the server and to
    reset the game state to the initial setup. Basic game functionality can
    now be simulated.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// ----- Defined Constants ----------------------------------------------------
+
+// const SERVER_URL = "https://essubia-backend.onrender.com/api";
+const SERVER_URL = "http://localhost:3000/api";
+
+// ----- Global Variables -----------------------------------------------------
+
+
+// ----- Main Function --------------------------------------------------------
+window.addEventListener('load', async function () {
+    document.getElementById('advanceTurn').addEventListener( 'click', async (pEvent) => {
+        let targetURL = SERVER_URL + '/turn';
+        await fetch( targetURL, {
+            method: 'POST',
+            headers: {
+                'Content-Length': '0'
+            }
+        });
+    });
+
+    document.getElementById('resetMap').addEventListener( 'click', async (pEvent) => {
+        let targetURL = SERVER_URL + '/reset';
+        await fetch( targetURL, {
+            method: 'POST',
+            headers: {
+                'Content-Length': '0'
+            }
+        });
+    });
+
+});
+
